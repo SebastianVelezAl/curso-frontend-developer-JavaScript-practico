d1b0a8cce2cccadf8b5780533d86b10c10a19c9d
Sebastian velez <sebasvelez714@gmail.com>
2023-07-02 22:05:28 -0500

    se incluye el modal de mobile

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuEmail =document.querySelector('.navbar-email');
 const desktopMenu =document.querySelector('.desktop-menu');
+const menuHamburguesa =document.querySelector('.menu');
+const mobileMenu =document.querySelector('.mobile-menu');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguesa.addEventListener('click',toggleMobileMenu)
 
 function toggleDesktopMenu(){
     console.log('clik');
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu(){
+    console.log('clicl calse mobile menu');
+    mobileMenu.classList.toggle('inactive');
 }
