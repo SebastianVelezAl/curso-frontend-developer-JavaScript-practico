76bbfc65e43192cdf4e6e3f5403fd7ca7731eda3
Sebastian velez <sebasvelez714@gmail.com>
2023-07-09 14:11:00 -0500

    Actualización de acciones de las ventanas

--- main.js
+++ main.js
@@ -1,17 +1,53 @@
 const menuEmail =document.querySelector('.navbar-email');
 const desktopMenu =document.querySelector('.desktop-menu');
 const menuHamburguesa =document.querySelector('.menu');
+const menuCarrito =document.querySelector('.navbar-shopping-cart');
 const mobileMenu =document.querySelector('.mobile-menu');
+const aside =document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamburguesa.addEventListener('click',toggleMobileMenu)
+menuCarrito.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu(){
     console.log('clik');
+    const isAsideClosed =aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.toggle('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+    
+
     desktopMenu.classList.toggle('inactive')
 }
 
-function toggleMobileMenu(){
-    console.log('clicl calse mobile menu');
+
+function toggleMobileMenu(){ 
+    console.log('clicl clase mobile menu');
+    const isAsideClosed =aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.toggle('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+
+
+function toggleCarritoAside(){
+    console.log('clik carrito de compras');
+    const mobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const menuDesktopClosed = desktopMenu.classList.contains('inactive');
+
+    if(!mobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    if(!menuDesktopClosed)
+    {
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+
+
 }
